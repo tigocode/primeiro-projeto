070fbefc6dad2a81b9bb3cb423e1bc4ed654fc93
tigocode <tiago.dantas16@live.com>
2024-10-16 00:06:30 -0300

    :recycle: refactor: implemetando metodologia RESET

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello world!');
+  let mensagem = {mensagem: 'Olá mundo!'};
+  res.json(mensagem);
 });
 
 app.listen(3333, () => {
